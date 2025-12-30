4b9d4ebbe7ec20d535898f2a6079fa82bd48c44c
Anthony Nguyen <skifreetony@yahoo.com>
2025-12-30 00:46:16 -0800

    delete distro

--- dist-electron/shared/ipc/result.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// shared/ipc/result.ts
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist-electron/shared/ipc/unwrap.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.unwrapIpcResult = unwrapIpcResult;
-function unwrapIpcResult(res) {
-    if (!res.ok) {
-        throw new Error(res.error.message);
-    }
-    return res.data;
-}
